9536a8f9aea62384eec595b3dbca781ea484cdee
icemanlc <icemanlc@mail.uc.edu>
2024-11-21 10:28:28 -0500

    uyfghtfc

--- InClass11212024/Index.aspx
+++ InClass11212024/Index.aspx
@@ -10,6 +10,8 @@
     <form id="form1" runat="server">
         <div>
             <asp:PlaceHolder ID="PlaceHolder1" runat="server"></asp:PlaceHolder>
+
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
         </div>
     </form>
 </body>
--- InClass11212024/Index.aspx.designer.cs
+++ InClass11212024/Index.aspx.designer.cs
@@ -31,5 +31,14 @@ namespace InClass11212024
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.PlaceHolder PlaceHolder1;
+
+        /// <summary>
+        /// Label1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
     }
 }
